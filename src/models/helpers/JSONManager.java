f2676e6c3e39a77d8893acccde69b4199298e0ff
JLaxy <laxamanajunell@gmail.com>
2024-06-10 10:07:34 +0800

    merge to latest version

--- src/models/helpers/JSONManager.java
+++ src/models/helpers/JSONManager.java
@@ -16,7 +16,6 @@ import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
