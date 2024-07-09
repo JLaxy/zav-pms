a7bb73b4e21f7c4024040ee34974baaf66484381
JLaxy <laxamanajunell@gmail.com>
2024-07-10 02:57:06 +0800

    attach periodic sales report to email
    - added ability to attach generated periodic report to email
    - prevent user to access auto report interval when auto email report destination is not set
    - working auto send and generate periodic report

--- src/models/modules/report/AutoReporter.java
+++ src/models/modules/report/AutoReporter.java
@@ -1,10 +1,15 @@
 package models.modules.report;
 
+import java.time.LocalDate;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import enums.ProgramSettings;
+import enums.ReportTimePeriods;
+import models.helpers.DateHelper;
 import models.helpers.JSONManager;
+import models.modules.Emailer;
+import models.schemas.User;
 
 public class AutoReporter {
     public static void setupAutoReporterTask() {
@@ -12,8 +17,38 @@ public class AutoReporter {
         TimerTask reporter = new TimerTask() {
             @Override
             public void run() {
-                System.out.println("auto report timer ran");
-                System.out.println("TODO: GENERATE THEN SEND REPORT TO EMAIL");
+                System.out.println("Running auto report timer");
+
+                // Finding time period
+                ReportTimePeriods.TimePeriod period = null;
+                if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
+                        .compareTo("Daily") == 0)
+                    period = ReportTimePeriods.TimePeriod.DAILY;
+                else if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
+                        .compareTo("Weekly") == 0)
+                    period = ReportTimePeriods.TimePeriod.WEEKLY;
+                else if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
+                        .compareTo("Monthly") == 0)
+                    period = ReportTimePeriods.TimePeriod.MONTHLY;
+
+                String fileName = new ReportManager(User.getSystemUser()).createDailyPeriodicSales(period,
+                        DateHelper.dateToString(LocalDate.now()));
+
+                if (fileName == null) {
+                    System.out.println("Failed to automatically generate automatic Periodic Sales Report!");
+                    return;
+                }
+
+                if (Emailer.sendReport(
+                        new JSONManager().getSetting(ProgramSettings.Setting.EMAIL_REPORT_DESTINATION.getValue()),
+                        fileName, User.getSystemUser(), fileName)) {
+                    System.out.println("Successfully sent email report to email \""
+                            + new JSONManager().getSetting(ProgramSettings.Setting.EMAIL_REPORT_DESTINATION.getValue())
+                            + "\"");
+                }
+                // Check interval
+                // Generate report
+                // Send to email
             }
         };
 
@@ -28,13 +63,13 @@ public class AutoReporter {
             System.out.println("no selected email report destination! auto reporter will not run.");
             return;
         } else if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
-                .compareTo("daily") == 0) {
+                .compareTo("Daily") == 0) {
             interval = 24 * 60 * 60 * 1000;
         } else if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
-                .compareTo("weekly") == 0) {
+                .compareTo("Weekly") == 0) {
             interval = 7L * 24 * 60 * 60 * 1000;
         } else if (new JSONManager().getSetting(ProgramSettings.Setting.REPORT_TIME_INTERVAL.getValue())
-                .compareTo("monthly") == 0) {
+                .compareTo("Monthly") == 0) {
             interval = 30L * 24 * 60 * 60 * 1000;
         }
 
--- src/models/report/ManualGenerateReportModel.java
+++ src/models/report/ManualGenerateReportModel.java
@@ -1,7 +1,16 @@
 package models.report;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.swing.JOptionPane;
+
 import controllers.report.ManualGenerateReportController;
 import enums.ReportTimePeriods;
+import enums.UserLogActions;
+import models.helpers.DateHelper;
+import models.helpers.PopupDialog;
+import models.modules.Emailer;
 import models.modules.report.ReportManager;
 import models.schemas.User;
 
@@ -14,6 +23,35 @@ public class ManualGenerateReportModel {
     }
 
     public boolean generatePeriodicReport(ReportTimePeriods.TimePeriod timePeriod, String date, User loggedInUser) {
-        return new ReportManager(loggedInUser).createDailyPeriodicSales(timePeriod, date);
+        String fileName = new ReportManager(loggedInUser).createDailyPeriodicSales(timePeriod, date);
+        try {
+            if (fileName != null) {
+                // Logging Action to Database
+                this.controller.getDBManager().query.logAction(loggedInUser.getId(), loggedInUser.getUname(),
+                        UserLogActions.Actions.GENERATED_PERIODIC_SALES_REPORT.getValue(),
+                        DateHelper.dateTimeToString(DateHelper.getCurrentDateTime()), "");
+                if (PopupDialog
+                        .confirmOperationDialog(
+                                "Report Generated! Do you want a copy to be sent to your email?") != JOptionPane.YES_OPTION)
+                    return true;
+                if (Emailer.sendReport(loggedInUser.getEmail(), timePeriod.getPeriodString() + " Periodic Sales Report",
+                        loggedInUser, fileName)) {
+
+                    // Logging Action to Database
+                    this.controller.getDBManager().query.logAction(loggedInUser.getId(), loggedInUser.getUname(),
+                            UserLogActions.Actions.SENT_GENERATED_REPORT_TO_EMAIL.getValue(),
+                            DateHelper.dateTimeToString(DateHelper.getCurrentDateTime()),
+                            "sent a copy of the report to email \"" + loggedInUser.getEmail() + "\"");
+
+                    PopupDialog.showInfoDialog("Report Sent",
+                            "Report successfully sent to Email Address \"" + loggedInUser.getEmail() + "\"");
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            PopupDialog.showErrorDialog(e, this.getClass().getName());
+            e.printStackTrace();
+        }
+        return false;
     }
 }
